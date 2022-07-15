0bc69007c03ff490c955792071c9c8ebf7212506
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2022-07-15 21:19:51 +0200

    fix arg parser

--- car_referencer/cli.py
+++ car_referencer/cli.py
@@ -16,37 +16,41 @@ def main():
     """Console script for car_referencer."""
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "c",
+        "-c",
         "--car_files",
-        helper="Collection of car files. Argument can contain wildcards.",
-        nargs="*",
+        help="Collection of car files. Argument can contain wildcards.",
+        nargs="+",
+        required=True,
     )
     parser.add_argument(
-        "p", "--parquet", helper="Parquet reference filename to be created."
+        "-p",
+        "--parquet",
+        help="Parquet reference filename to be created.",
+        required=True,
     )
     parser.add_argument(
-        "i",
+        "-i",
         "--index",
-        helper="Index file to be created. Helps for temporary steps.",
+        help="Index file to be created. Helps for temporary steps.",
         default=None,
     )
     parser.add_argument(
-        "r",
+        "-r",
         "--cid",
-        helper="Root CID. Recursive references of this CID will be added to the reference filesystem",
+        help="Root CID. Recursive references of this CID will be added to the reference filesystem",
+        required=True,
     )
     args = parser.parse_args()
-
-    if os.path.exists(args["index"]):
-        logging.info(f"Index file {args['index']} exists and will be used.")
-        index_df = pd.read_parquet(args["index"])
+    if os.path.exists(args.index):
+        logging.info(f"Index file {args.index} exists and will be used.")
+        index_df = pd.read_parquet(args.index)
     else:
         logging.info("Creating index of available CIDs in given car files")
-        index_df = idx.generate_index(glob.glob(args["car_files"]), args["index"])
+        index_df = idx.generate_index(glob.glob(args.car_files), args.index)
 
     logging.info("Create reference filesystem for {args['cid']}.")
 
-    _ = ref.create_preffs(CID.decode(args["cid"]), index_df, parquet_fn=args["parquet"])
+    _ = ref.create_preffs(CID.decode(args.cid), index_df, parquet_fn=args.parquet)
 
     return 0
 

ddf3e3462a3e4ce0f48fa9a8bba24981ab3053ef
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2022-07-16 11:46:12 +0200

    add pytest fixtures for sample zarr/car files

--- tests/test_car_referencer.py
+++ tests/test_car_referencer.py
@@ -2,6 +2,8 @@
 
 """Tests for `car_referencer` package."""
 
+import glob
+import os
 import subprocess
 
 import pytest
@@ -10,17 +12,27 @@ import xarray as xr
 from car_referencer import car_referencer
 
 
-def create_test_zarr():
+def create_test_zarr(tmp_path):
     xr.tutorial.load_dataset("air_temperature").chunk(
         {"time": 100, "lat": 5, "lon": 5}
-    ).to_zarr("example.zarr")
+    ).to_zarr(str(tmp_path) + "/example.zarr")
 
 
-def create_test_cars(zarr_fn):
-    subprocess.call("echo {} > old-example.json")
-    subprocess.call(
-        "~/go/bin/linux2ipfs -car-size 3382286 -driver car-example.%d.car -incremental-file old-example.json example.zarr"
+@pytest.fixture
+def test_cars(tmp_path):
+    create_test_zarr(tmp_path)
+    with open(f"{str(tmp_path)}/old-example.json", "w") as f:
+        f.write("{}")
+    if os.environ.get("GOPATH"):
+        gopath = os.environ["GOPATH"] + "/go/bin"
+    else:
+        gopath = os.environ["HOME"] + "/go/bin"
+    assert os.path.exists(gopath + "/linux2ipfs"), "linux2ipfs could not be found"
+    subprocess.check_output(
+        f"{gopath}/linux2ipfs -car-size 3382286 -driver car-{str(tmp_path)}/example.%d.car -incremental-file {str(tmp_path)}/old-example.json {str(tmp_path)}/example.zarr",
+        shell=True,
     )
+    return sorted(glob.glob(f"{str(tmp_path)}/example.*.car"))
 
 
 @pytest.fixture

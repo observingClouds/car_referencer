eb695ea2d82092e8c00102a506f61b76bbb3fb97
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2022-07-16 11:49:02 +0200

    WIP: index creation and preffs creation tests

--- tests/test_car_referencer.py
+++ tests/test_car_referencer.py
@@ -49,3 +49,11 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+
+
+def test_index_creation(test_cars):
+    print(test_cars)
+
+
+def test_preffs_creation():
+    pass

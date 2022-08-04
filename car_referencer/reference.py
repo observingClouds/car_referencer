c160cc82d56e44921b77c526bac482cec47f0d9e
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2022-08-05 00:15:03 +0200

    Rewrite of loop_create_reffs incl. fixes
    Adapt function to read test car QmfZwnbqm2fmfBLtcfWT7fdr3VaPUB4fMB6SsypkiMNRFV

--- car_referencer/reference.py
+++ car_referencer/reference.py
@@ -4,6 +4,8 @@ import pandas as pd
 from ipldstore.unixfsv1 import Data, PBNode
 from multiformats import CID
 
+verbose = False
+
 
 def load_data(data):
     node = PBNode.loads(data)
@@ -25,67 +27,74 @@ def list_links(node):
     return d
 
 
-def get_size(blocksizes, n, entry):
-    if blocksizes != []:
-        offset = entry.offset + sum(blocksizes[0:n])
-        size = blocksizes[n]
-    else:
-        offset = entry.offset
-        size = entry["size"]
-    return offset, size
-
-
 def loop_create_reffs(cid, index, ref_fs=[], dir=None):
-    node, data = create_reference_fs(cid, index)
-    for n, (name, hash) in enumerate(list_links(node)):
-        if hash.hashfun.code == 18:  # raw
-            if hash.codec.name == "dag-pb":
-                ref_fs = loop_create_reffs(
-                    hash, index, ref_fs, dir="/".join(filter(bool, [dir, name]))
+    if cid.codec.name == "raw":
+        if cid.hashfun.codec.name == "identity":
+            if verbose:
+                print(
+                    dir,
+                    base64.b64decode(base64.b64encode(cid.raw_digest).decode("ascii")),
                 )
-            entry = index.loc[hash.digest]
-            if isinstance(entry, pd.DataFrame):
-                entry = entry.sort_values("order")
-                ref_fs.extend(
-                    [
-                        [
-                            "/".join(filter(bool, [dir, name])),
-                            e.file,
-                            # e.offset,
-                            # e["size"],
-                            get_size(data.blocksizes, n, e)[0],
-                            get_size(data.blocksizes, n, e)[1],
-                            None,
-                        ]
-                        for _, e in entry.iterrows()
-                    ]
-                )
-            else:  # single entry per zarr-file
-                ref_fs.append(
-                    [
-                        "/".join(filter(bool, [dir, name])),
-                        entry.file,
-                        # entry.offset,
-                        # entry["size"],
-                        get_size(data.blocksizes, n, entry)[0],
-                        get_size(data.blocksizes, n, entry)[1],
-                        None,
-                    ]
-                )
-        elif hash.hashfun.code == 0:  # identity
             ref_fs.append(
                 [
-                    "/".join(filter(bool, [dir, name])),
+                    dir,
                     None,
                     None,
                     None,
-                    base64.b64decode(base64.b64encode(hash.raw_digest).decode("ascii")),
+                    base64.b64decode(base64.b64encode(cid.raw_digest).decode("ascii")),
                 ]
             )
         else:
-            raise ValueError(
-                f"Found unknown hash code {hash.hashfun.code}, {name}, {hash.codec.name}"
+            if verbose:
+                print(dir, index.loc[cid.digest])
+            entry = index.loc[cid.digest]
+            ref_fs.extend(
+                [
+                    [
+                        dir,
+                        entry.file,
+                        entry.offset,
+                        entry["size"],
+                        None,
+                    ]
+                ]
             )
+    else:
+        node, data = create_reference_fs(cid, index)
+        len_links = len(list_links(node))
+        if cid.codec.name == "dag-pb" and len_links > 0:
+            for n, (name, hash) in enumerate(list_links(node)):
+                ref_fs = loop_create_reffs(
+                    hash, index, ref_fs, dir="/".join(filter(bool, [dir, name]))
+                )
+        elif cid.codec.name == "dag-pb" and len_links == 0:
+            if verbose:
+                print(dir, data.Data)
+            # ref_fs.extend(
+            #                    [
+            #                        [
+            #                            dir,
+            #                            None,
+            #                            None,
+            #                            None,
+            #                            data.Data,
+            #                        ]
+            #                    ]
+            #                ) # or use entry = index.loc[cid.digest] and cut of header in entry
+            entry = index.loc[cid.digest]
+            ref_fs.extend(
+                [
+                    [
+                        dir,
+                        entry["file"],
+                        entry["offset"] + 6,
+                        data.filesize,
+                        None,
+                    ]
+                ]
+            )
+        else:
+            raise ValueError(f"cid codec {cid.codec.name} not known")
     return ref_fs
 
 
